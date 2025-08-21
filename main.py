ed808924ae2d355b3b6754a686abd58fe8853e21
Sofi <110249809+Cl1cker0@users.noreply.github.com>
2025-08-20 20:31:06 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import asyncio
+import threading
+import time
+from keep_alive import keep_alive
+from discord import listener
+from src.roblox import roblox_main
+
+keep_alive()
+# https://github.com/notasnek/roblox-autojoiner
+# буду рад звезде на репозитории / please STAR my repo
+
+
+if __name__ == "__main__":
+    print("Roblox AutoJoiner for Chilli's Notify by notasnek")
+    print("Github: https://github.com/notasnek/roblox-autojoiner")
+    print("Join our discord: https://discord.gg/fQSP3VFks9")
+    print("Version: 1.0.3")
+    print("Starting in 2 seconds...")
+    print()
+
+    # убирая мое авторство вы нарушаете лицензию (читай LICENSE.md)
+    # By removing my authorship, you are violating the license (read LICENSE.md)
+    # notasnek
+
+    time.sleep(2)
+
+    threading.Thread(target=roblox_main, daemon=True).start()
+    asyncio.run(listener())
+    keep_alive()
+
+
+# https://github.com/notasnek/roblox-autojoiner
